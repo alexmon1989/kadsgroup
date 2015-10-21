b9429abc5bab5eb63023827e3cbbd6117a14c1e7
alexmon1989 <alex.mon1989@gmail.com>
2015-10-21 11:31:07 +0300

    fix: Ссылка на дом. страницу в логотипе в футере.

--- resources/views/marketing/widgets/footer_about.blade.php
+++ resources/views/marketing/widgets/footer_about.blade.php
@@ -1,6 +1,6 @@
 <!-- About -->
 <div class="col-md-3 md-margin-bottom-40">
-    <a href="index.html"><img id="logo-footer" class="footer-logo" src="assets/img/logo-kadsgroup-footer.png" alt="{{ url() }}"></a>
+    <a href="{{ url() }}"><img id="logo-footer" class="footer-logo" src="assets/img/logo-kadsgroup-footer.png" alt="{{ url() }}"></a>
     {!! $text !!}
 </div><!--/col-md-3-->
 <!-- End About -->
